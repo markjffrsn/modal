adefedfc2329e058ba56011af5c489d440f5062c
Mark Jefferson <markjeffersonhidalgo08@gmail.com>
2024-06-18 15:09:37 +0800

    initial commit

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const closeBtn = document.querySelector(".close-btn");
 const openBtn = document.querySelector(".open-btn");
 const backDrop = document.querySelector(".modal-backdrop");
 const modalWrapper = document.querySelector(".modal");
+const btns = document.querySelectorAll(".btn");
 
-closeBtn.addEventListener("click", () => {
-  backDrop.style.display = "none";
-  modalWrapper.style.display = "block";
-});
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const currentButton = e.currentTarget.classList;
+    console.log(currentButton);
 
-openBtn.addEventListener("click", () => {
-  backDrop.style.display = "grid";
-  modalWrapper.style.display = "none";
+    if (currentButton.contains("close-btn")) {
+      backDrop.style.display = "none";
+      modalWrapper.style.display = "block";
+    } else if (currentButton.contains("open-btn")) {
+      backDrop.style.display = "grid";
+      modalWrapper.style.display = "none";
+    }
+  });
 });
